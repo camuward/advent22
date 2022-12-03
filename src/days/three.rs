19e80daf5aa218bdff4a3f1d1eacdd185d1391ec
Cameron Ward <cameronuward@gmail.com>
2022-12-03 16:00:32 -0700

    rename day three variables

--- src/days/three.rs
+++ src/days/three.rs
@@ -1,26 +1,24 @@
-fn unique_items(s: &str) -> u64 {
-    s.bytes()
-        .map(|b| match b {
-            b'a'..=b'z' => 1 + b - b'a',
-            b'A'..=b'Z' => 27 + b - b'A',
+fn unique_items(sect: &str) -> u64 {
+    sect.bytes()
+        .map(|c| match c {
+            b'a'..=b'z' => 1 + c - b'a',
+            b'A'..=b'Z' => 27 + c - b'A',
             _ => unreachable!(),
         })
-        .fold(0, |acc, b| acc | (1u64 << b))
+        .fold(0u64, |acc, n| acc | (1 << n))
 }
 
-pub fn one(input: &str) -> u32 {
-    input
-        .lines()
+pub fn one(bags: &str) -> u32 {
+    bags.lines()
         .map(|bag| bag.split_at(bag.len() / 2))
         .map(|(l, r)| [l, r].map(unique_items))
         .map(|[l, r]| u64::trailing_zeros(l & r))
         .sum()
 }
 
-pub fn two(input: &str) -> u32 {
-    input
-        .lines()
-        .array_chunks::<3>()
+pub fn two(bags: &str) -> u32 {
+    bags.lines()
+        .array_chunks::<3>() // unstable
         .map(|bags| bags.map(unique_items))
         .map(|[a, b, c]| a & b & c)
         .map(u64::trailing_zeros)
