d9e7354159e93b6b5a71d402a650b0a326670d65
Cameron Ward <cameronuward@gmail.com>
2022-12-02 20:48:08 -0700

    refactor day two

--- src/days/two.rs
+++ src/days/two.rs
@@ -1,31 +1,32 @@
-fn split_line(input: &str) -> impl Iterator<Item = (u8, u8)> + '_ {
-    let left = input.bytes();
-    let right = input.bytes().skip(2);
-
-    left.zip(right).step_by(4)
-}
-
 pub fn one(input: &str) -> u32 {
-    split_line(input)
+    input
+        .as_bytes()
+        .chunks(4)
+        .map(|ln| (ln[0], ln[2]))
         .map(|(left, right)| {
-            let score = match (left as char, right as char) {
+            let choice_pts = right - b'W';
+            let win_pts = match (left as char, right as char) {
                 ('A', 'X') | ('B', 'Y') | ('C', 'Z') => 3,
                 ('A', 'Y') | ('B', 'Z') | ('C', 'X') => 6,
                 ('A', 'Z') | ('B', 'X') | ('C', 'Y') => 0,
                 _ => unreachable!(),
             };
 
-            score + (right - b'W') as u32
+            (win_pts + choice_pts) as u32
         })
         .sum()
 }
 
 pub fn two(input: &str) -> u32 {
-    split_line(input)
-        .map(|(l, r)| (l - b'A', r - b'X'))
+    input
+        .as_bytes()
+        .chunks(4)
+        .map(|ln| (ln[0] - b'A', ln[2] - b'X'))
         .map(|(left, right)| {
-            let choice = (2 + left + right) % 3 + 1;
-            (3 * right + choice) as u32
+            let choice_pts = (2 + left + right) % 3 + 1;
+            let win_pts = 3 * right;
+
+            (win_pts + choice_pts) as u32
         })
         .sum()
 }
