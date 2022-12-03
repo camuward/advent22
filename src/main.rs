9044788266afea3874e76dde9b59f985613c1984
Cameron Ward <cameronuward@gmail.com>
2022-12-02 17:06:04 -0700

    add timer to `main`

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use days::one as current;
+use std::time::Instant;
 
 mod days;
 
 fn main() -> anyhow::Result<()> {
     let input = std::fs::read_to_string("input.txt")?;
 
-    println!("{}", current::one(&input));
-    println!("{}", current::two(&input));
+    use days::two as current;
+    for func in [current::one, current::two] {
+        let start = Instant::now();
+        let res = func(&input);
+        let dur = start.elapsed().as_nanos();
+
+        println!("{res} ({dur} ns)");
+    }
 
     Ok(())
 }
