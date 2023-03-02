8efcaebcd43f3bcb8b6e1c295365be4e180fd0d7
Cameron Ward <cameronuward@gmail.com>
2023-03-01 17:23:51 -0700

    use inline module

--- src/days/mod.rs
+++
@@ -1,5 +0,0 @@
-#![allow(dead_code)]
-
-pub mod one;
-pub mod two;
-pub mod three;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![feature(array_chunks, iter_array_chunks)]
 
-mod days;
+mod days {
+    pub mod one;
+    pub mod two;
+    pub mod three;
+}
 
 fn main() -> anyhow::Result<()> {
     let input = std::fs::read_to_string("input.txt")?;
